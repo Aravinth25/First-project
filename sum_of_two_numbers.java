8cb755f4fe164ef836b291de057b4c963fc544eb
aravinth <aravinth2511@gmail.com>
2021-07-23 09:47:48 +0530

    addition

---
+++ sum_of_two_numbers.java
@@ -0,0 +1,8 @@
+public class sum_of_two_numbers{
+  public static void main(String[] args)
+  {
+    int num1 = 5 , num2 = 10, sum;
+    sum = num1 + num2;
+    System.out.println("sum of these numbers are :"+sum);
+  }
+}
